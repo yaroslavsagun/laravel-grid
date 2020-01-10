fbdc34cba3d72a3e031ef91e49c577c0f95ebfb9
Roman Chaikovskyi <r.chaykovskiy@gmail.com>
2020-01-10 14:26:36 +0200

    Added multiple selection and bulk delete. UI changes.

--- src/resources/views/grid/filter.blade.php
+++ src/resources/views/grid/filter.blade.php
@@ -8,15 +8,15 @@
             {!! $col->filter->render(['titleSetOnColumn' => $col->filterTitle]) !!}
         </th>
     @endif
-    @if($loop->last)
-        <th class="{{ $grid->getGridFilterFieldColumnClass() }}">
-            <div class="float-right">
-                <button type="submit"
-                        class="btn btn-outline-primary grid-filter-button"
-                        title="filter data"
-                        form="{{ $formId }}">Filter&nbsp;<i class="fa fa-filter"></i>
-                </button>
-            </div>
-        </th>
-    @endif
+    {{--@if($loop->last)--}}
+        {{--<th class="{{ $grid->getGridFilterFieldColumnClass() }}">--}}
+            {{--<div class="float-right">--}}
+                {{--<button type="submit"--}}
+                        {{--class="btn btn-outline-primary grid-filter-button"--}}
+                        {{--title="filter data"--}}
+                        {{--form="{{ $formId }}">Filter&nbsp;<i class="fa fa-filter"></i>--}}
+                {{--</button>--}}
+            {{--</div>--}}
+        {{--</th>--}}
+    {{--@endif--}}
 @endforeach
--- src/resources/views/grid/grid.blade.php
+++ src/resources/views/grid/grid.blade.php
@@ -1,36 +1,58 @@
 @extends($grid->getRenderingTemplateToUse())
 @section('data')
     <div class="row">
-        @if($grid->shouldRenderSearchForm())
-            {!! $grid->renderSearchForm() !!}
-        @endif
+        <div class="col-md-{{ $grid->getGridToolbarSize()[0] }}">
+            <button id="grid-select-all" type="button" class="btn btn-success" title="Select all items on page">
+                <i class="fa fa-square"></i>
+                Select All
+            </button>
 
+            <a href="{{ $grid->getMultipleDeleteUrl() }}" id="grid-delete-selected" class="data-remote btn btn-danger disabled" data-trigger-pjax="1" data-trigger-confirm="1" data-bulk-action="1" data-pjax-target="#product-grid">
+                <i class="fa fa-trash-alt"></i>
+                Delete Selected
+            </a>
+
+            @if($grid->shouldRenderSearchForm())
+                {!! $grid->renderSearchForm() !!}
+            @endif
+        </div>
         @if($grid->hasButtons('toolbar'))
             <div class="col-md-{{ $grid->getGridToolbarSize()[1] }}">
                 <div class="float-right">
-                    <label for="grid-filter-page_size" title="Applies with filters">Page Size: </label>
-                    <select name="page_size" id="grid-filter-page_size" form="{{ $grid->getFilterFormId() }}" class="" title="Applies with filters">
-                        @foreach([50 => 50, 100 => 100, 200 => 200, 500 => 500] as $k => $v)
-                            @if((request('page_size') !== null && request('page_size') == $k) || $grid->getGridPaginationPageSize() == $k)
-                                <option value="{{ $k }}" selected>{{ $v }}</option>
-                            @else
-                                <option value="{{ $k }}">{{ $v }}</option>
-                            @endif
-                        @endforeach
-                    </select>
                     @foreach($grid->getButtons('toolbar') as $button)
                         {!! $button->render() !!}
                     @endforeach
+
+                    <div class="page-size-wrapper">
+                        <label for="grid-filter-page_size" title="Applies with filters">Page Size: </label>
+                        <select name="page_size" id="grid-filter-page_size" form="{{ $grid->getFilterFormId() }}" class=""
+                                title="Applies with filters">
+                            @foreach([50 => 50, 100 => 100, 200 => 200, 500 => 500] as $k => $v)
+                                @if((request('page_size') !== null && request('page_size') == $k) || $grid->getGridPaginationPageSize() == $k)
+                                    <option value="{{ $k }}" selected>{{ $v }}</option>
+                                @else
+                                    <option value="{{ $k }}">{{ $v }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <button type="submit"
+                            class="btn btn-info grid-filter-button"
+                            title="Filter grid results by applied criterias"
+                            form="{{ $grid->getFilterFormId() }}">Apply Filters&nbsp;<i class="fa fa-filter"></i>
+                    </button>
                 </div>
             </div>
         @endif
-    </div>
 
+    </div>
     <form action="{{ $grid->getSearchUrl() }}" method="GET" id="{{ $grid->getFilterFormId() }}"></form>
     <div class="table-responsive grid-wrapper">
         <table class="{{ $grid->getClass() }}">
             <thead class="{{ $grid->getHeaderClass() }}">
             <tr class="filter-header">
+                <th></th>
                 @foreach($columns as $column)
 
                     @if($loop->first)
@@ -82,10 +104,11 @@
                         @endif
                     @endif
                 @endforeach
-                <th></th>
+                {{--<th></th>--}}
             </tr>
             @if($grid->shouldRenderGridFilters())
                 <tr>
+                    <td></td>
                     {!! $grid->renderGridFilters() !!}
                 </tr>
             @endif
@@ -113,41 +136,64 @@
                         @endphp
                         <tr class="{{ $trClassCallback }}">
                             @endif
+                            <td class="selection"><input data-id="{{ $item->id }}" type="checkbox"/></td>
                             @foreach($columns as $column)
+                                @if($column->isLinkable())
+                                    @php
+                                        $columnCallback = call_user_func($grid->getLinkableCallback(), $grid->transformName(), $item);
+                                    @endphp
+                                @endif
+
                                 @if(is_callable($column->data))
                                     @if($column->useRawFormat)
                                         <td class="{{ $column->rowClass }}">
-                                            {!! call_user_func($column->data, $item, $column->key) !!}
+                                            @if($column->isLinkable())
+                                                <a href="{{ $columnCallback }}">{!! call_user_func($column->data, $item, $column->key) !!}</a>
+                                            @else
+                                                {!! call_user_func($column->data, $item, $column->key) !!}
+                                            @endif
                                         </td>
                                     @else
                                         <td class="{{ $column->rowClass }}">
-                                            {{ call_user_func($column->data , $item, $column->key) }}
+                                            @if($column->isLinkable())
+                                                <a href="{{ $columnCallback }}">{{ call_user_func($column->data, $item, $column->key) }}</a>
+                                            @else
+                                                {{ call_user_func($column->data, $item, $column->key) }}
+                                            @endif
                                         </td>
                                     @endif
                                 @else
                                     @if($column->useRawFormat)
                                         <td class="{{ $column->rowClass }}">
-                                            {!! $item->{$column->key} !!}
+                                            @if($column->isLinkable())
+                                                <a href="{{ $columnCallback }}">{!! $item->{$column->key} !!}</a>
+                                            @else
+                                                {!! $item->{$column->key} !!}
+                                            @endif
                                         </td>
                                     @else
                                         <td class="{{ $column->rowClass }}">
-                                            {{ $item->{$column->key} }}
+                                            @if($column->isLinkable())
+                                                <a href="{{ $columnCallback }}">{{ $item->{$column->key} }}</a>
+                                            @else
+                                                {{ $item->{$column->key} }}
+                                            @endif
                                         </td>
                                     @endif
                                 @endif
-                                @if($loop->last && $grid->hasButtons('rows'))
-                                    <td>
-                                        <div class="float-right">
-                                            @foreach($grid->getButtons('rows') as $button)
-                                                @if(call_user_func($button->renderIf, $grid->transformName(), $item))
-                                                    {!! $button->render(['gridName' => $grid->transformName(), 'gridItem' => $item]) !!}
-                                                @else
-                                                    @continue
-                                                @endif
-                                            @endforeach
-                                        </div>
-                                    </td>
-                                @endif
+                                {{--@if($loop->last && $grid->hasButtons('rows'))--}}
+                                {{--<td>--}}
+                                {{--<div class="float-right">--}}
+                                {{--@foreach($grid->getButtons('rows') as $button)--}}
+                                {{--@if(call_user_func($button->renderIf, $grid->transformName(), $item))--}}
+                                {{--{!! $button->render(['gridName' => $grid->transformName(), 'gridItem' => $item]) !!}--}}
+                                {{--@else--}}
+                                {{--@continue--}}
+                                {{--@endif--}}
+                                {{--@endforeach--}}
+                                {{--</div>--}}
+                                {{--</td>--}}
+                                {{--@endif--}}
                             @endforeach
                         </tr>
                         @endforeach
@@ -174,25 +220,25 @@
 @endsection
 @push('grid_js')
     <script>
-      (function($) {
-        let grid = "{{ '#' . $grid->getId() }}";
-        let filterForm = "{{ '#' . $grid->getFilterFormId() }}";
-        let searchForm = "{{ '#' . $grid->getSearchFormId() }}";
-        _grids.grid.init({
-          id: grid,
-          filterForm: filterForm,
-          dateRangeSelector: '.date-range',
-          searchForm: searchForm,
-          pjax: {
-            pjaxOptions: {
-              scrollTo: false,
-            },
-            // what to do after a PJAX request. Js plugins have to be re-intialized
-            afterPjax: function(e) {
-              _grids.init();
-            },
-          },
-        });
-      })(jQuery);
+        (function ($) {
+            let grid = "{{ '#' . $grid->getId() }}";
+            let filterForm = "{{ '#' . $grid->getFilterFormId() }}";
+            let searchForm = "{{ '#' . $grid->getSearchFormId() }}";
+            _grids.grid.init({
+                id: grid,
+                filterForm: filterForm,
+                dateRangeSelector: '.date-range',
+                searchForm: searchForm,
+                pjax: {
+                    pjaxOptions: {
+                        scrollTo: false,
+                    },
+                    // what to do after a PJAX request. Js plugins have to be re-intialized
+                    afterPjax: function (e) {
+                        _grids.init();
+                    },
+                },
+            });
+        })(jQuery);
     </script>
 @endpush
